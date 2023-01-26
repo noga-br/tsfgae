ccef6bc9a2de0979ae438536c7c8739bd46ecfde
Noga Barnea <nogabarnea10@gmail.com>
2023-01-26 21:21:06 +0200

    firstQuestion

--- tsfgae.js
+++ tsfgae.js
@@ -2,8 +2,8 @@ function activeChess() {
     //document.getElementById("body").className = "chessQuetionBackround";
    // document.getElementById("chess").className == document.getElementById("hide").className;
     //document.getElementById("sfgaeChessPage").className = "show";
-    alert("get here!");
     document.getElementById("body").className = "sfgaeChessPage";
     document.getElementById("maPage").className = "hide";
     document.getElementById("sfgaeChessPage").className = "show";
+    alert("You have found the box!");
 }
