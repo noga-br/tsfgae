5b8c782c597387291406e870a033445ffe95d6d1
Noga Barnea <nogabarnea10@gmail.com>
2023-01-26 16:22:16 +0200

    addFirstJs

---
+++ tsfgae.js
@@ -0,0 +1,5 @@
+function activeChess() {
+    document.getElementById("body").className = "chessQuetionBackround";
+    document.getElementById("chess").className = "hide";
+    document.getElementById("sfgaeChessPage").className = "show";
+}
