60aec3f53ef79933ebe98f19c4dfd6f3e2950b7e
Noga Barnea <nogabarnea10@gmail.com>
2023-01-26 22:31:36 +0200

    Q1 - half way

--- tsfgae.js
+++ tsfgae.js
@@ -1,9 +1,31 @@
 function activeChess() {
-    //document.getElementById("body").className = "chessQuetionBackround";
-   // document.getElementById("chess").className == document.getElementById("hide").className;
-    //document.getElementById("sfgaeChessPage").className = "show";
     document.getElementById("body").className = "sfgaeChessPage";
     document.getElementById("maPage").className = "hide";
     document.getElementById("sfgaeChessPage").className = "show";
     alert("You have found the box!");
+}
+
+function coverImgCheck(coverNum) {
+    document.getElementById("cover" + coverNum).classList.add("imgCoverOnclick");
+    // document.querySelector("#coverImage").disabled = true;
+    document.getElementById("coverImage").classList.add("hide");
+    checkCoverNum(coverNum)
+}
+
+function checkCoverNum(coverNum) {
+    if (coverNum == 2) {
+        document.getElementById("coverQuetionNum").innerHTML("4");
+    }
+    else if(coverNum == 1) {
+        document.getElementById("coverQuetionNum").innerHTML("5");
+    }
+    else if(coverNum == 3) {
+        document.getElementById("coverQuetionNum").innerHTML("6");
+    }
+    else if(coverNum == 4) {
+        document.getElementById("coverQuetionNum").innerHTML("7");
+    }
+    else if(coverNum == 5) {
+        document.getElementById("coverQuetionNum").innerHTML("8");
+    }
 }
