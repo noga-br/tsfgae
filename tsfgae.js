8f1a89f7b4db85777d8057b2bd60d6e06430cc0e
noga-br <noga.ronenbu@gmail.com>
2023-01-26 19:45:26 +0200

    the great update

--- tsfgae.js
+++ tsfgae.js
@@ -1,6 +1,9 @@
 function activeChess() {
-    alert ("hello");
-    document.getElementById("body").className = "chessQuetionBackround";
-    document.getElementById("chess").className == document.getElementById("hide").className;
+    //document.getElementById("body").className = "chessQuetionBackround";
+   // document.getElementById("chess").className == document.getElementById("hide").className;
+    //document.getElementById("sfgaeChessPage").className = "show";
+    alert("get here!");
+    document.getElementById("body").className = "sfgaeChessPage";
+    document.getElementById("maPage").className = "hide";
     document.getElementById("sfgaeChessPage").className = "show";
 }
